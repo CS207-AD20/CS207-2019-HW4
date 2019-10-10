85096d416a5a8f7055d44255b77ea14b3436a2d0
lindseysbrown <lindseysbrown@hotmail.com>
2019-10-10 13:18:32 -0400

    Edits to text of command line visualization

--- HW4vis.py
+++ HW4vis.py
@@ -32,6 +32,8 @@ def get_f():
         func = get_f()
     return func
 
+print('AUTOMATIC DIFFERENTIATION VISUALIZATION\n')
+
 
 x = get_x()
 y = get_y()
@@ -42,20 +44,29 @@ f = func(x, y)
 print('The value of your function at ({},{}) is {}'.format(x.val, y.val, f.val))
 print('The derivative of your function at ({},{}) is {}'.format(x.val, y.val, f.der))
 
-G, edge_labs, pos, labs = ADgraph.get_graph_setup(f)
 
-input('We will now visualize the forward mode graph of your function. Close the graph window when you are done.  Hit Enter to continue.')
+print('\n\nFORWARD MODE AUTOMATIC DIFFERENTIATION\n')
 
-ADgraph.draw_graph2(f, G, edge_labs, pos, labs)
-
-print('The corresponding computational table for forward mode is:')
+print('The computational table for forward mode of your function is:')
 table = ADgraph.gen_table(f)
 print(table)
 
-input('Now let\'s think about reverse mode.  We will first visualize the entire reverse graph.  (This is the graph you should save for HW4.)  Close the graph window when you are done.  Hit Enter to continue.')
+G, edge_labs, pos, labs = ADgraph.get_graph_setup(f)
+
+input('\nWe can visualize the corresponding forward mode graph of your function. (Hint: Maximizing the graph window may make the graph easier to read.) When you are done looking at the graph, close the plot window to proceed to reverse mode.  Hit Enter to continue.')
+
+ADgraph.draw_graph2(f, G, edge_labs, pos, labs)
+
+print('\n\n\nREVERSE MODE AUTOMATIC DIFFERENTIATION\n')
+
+input('Now let\'s think about reverse mode.  We will first visualize the entire reverse graph.  (This is the graph you should save for HW4.)  When you are done, close the plot window to proceed.  Hit Enter to continue.')
 ADgraph.draw_graph_rev2(f, G, edge_labs, pos, labs)
 
-input('Let\'s step through the graph traversal for using reverse mode to find the partial derivative with respect to x.  Hit Enter to continue.')
+input('\nLet\'s step through the graph traversal for using reverse mode to find the partial derivative with respect to x.  When the plot window launches, click on it, and follow the instructions to step through the calculation (using the arrowkeys on the keyboard).  When you are done, close the plot window.  Hit Enter to continue.')
 ADgraph.draw_graph_rev_dynamic(f, x.revder(f)[1], G, edge_labs, pos, labs, x.revder(f)[0])
 
 
+input('\nLet\'s step through the graph traversal for using reverse mode to find the partial derivative with respect to y.  When the plot window launches, click on it, and follow the instructions to step through the calculation (using the arrowkeys on the keyboard).  When you are done, close the plot window.  Hit Enter to continue.')
+ADgraph.draw_graph_rev_dynamic(f, y.revder(f)[1], G, edge_labs, pos, labs, y.revder(f)[0])
+
+print('\nYou have completed the visualization of this function.  Run the script again if you want to try a different function.')
